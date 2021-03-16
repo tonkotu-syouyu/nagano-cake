9c9ea81be9713a9d316815ba20bcfec0a109f586
Sayo <76867260+Sayo-Shiozuka@users.noreply.github.com>
2021-03-16 11:23:10 +0900

    Revert "Add public controller"

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,4 @@
 Rails.application.routes.draw do
   devise_for :users
-
-  #namespace :public do
-    root to: 'homes#top'
-    get '/about', to: 'homes#about'
-    resources :products, only:[:index, :show]
-    #resources :registrations, only:[:new, :create]
-    #resources :sessions, only:[:new, :create, :destroy]
-    resources :customers, only:[:edit, :update]
-      get '/customers/my_page', to: 'customers#show'
-      get 'customers/unsubscribe'
-      patch 'customers/withdraw'
-    resources :cart_products, only:[:index, :update, :destroy, :create]
-      delete 'cart_products/destroy_all'
-    resources :orders, only:[:new, :create, :index, :show]
-      post 'orders/confirm'
-      get 'orders/complete'
-    resources :addresses, only:[:index, :edit, :create, :update, :destroy]
-  #end
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  #get 'top' => 'public/homes#top'
 end
--- test/controllers/homes_controller_test.rb
+++ test/controllers/homes_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class Public::CartItemsControllerTest < ActionDispatch::IntegrationTest
+class HomesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
