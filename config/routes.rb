ffff81d466ca72396f2953b162b62b738fb65368
kyohei775 <mr.rad.ok1201@gmail.com>
2021-03-16 02:30:11 +0000

    addAdminController

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :users
+  
+   # admin
+    devise_for :admins
+  namespace :admin do
+    root 'homes#top'
+    get 'homes/top'
+    resources :sessions, only: [:new, :create, :destroy]
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :orders, only: [:show, :update]
+    resources :orders_details, only: [:update]
+  end
+  
+  devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
