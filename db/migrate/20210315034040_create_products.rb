0300b2a0b98cf2d1b11d56d7b750c1a099a1bc7d
EC2 Default User <ec2-user@ip-172-31-26-240.ec2.internal>
2021-03-16 02:36:35 +0000

    add column

--- db/migrate/20210315034040_create_products.rb
+++ db/migrate/20210315034040_create_products.rb
@@ -1,6 +1,15 @@
 class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
+      
+      t.integer :genre_id
+      
+      t.boolean :is_active, defalut: true
+      
+      t.string :name
+      t.integer :price
+      t.text :introduction
+      t.string :image_id
 
       t.timestamps
     end
--- db/migrate/20210315034112_create_orders.rb
+++ db/migrate/20210315034112_create_orders.rb
@@ -1,7 +1,21 @@
 class CreateOrders < ActiveRecord::Migration[5.2]
   def change
     create_table :orders do |t|
-
+      
+      t.integer :customer_id
+      t.string :postal_code
+      t.string :address
+      t.string :name
+      
+      # enumで管理
+      t.integer :status, defalut: 0
+      
+      t.integer :total_payment
+      t.integer :shipping_cost
+      
+      # enumで管理
+      t.integer :payment_method, defalut: 0
+      
       t.timestamps
     end
   end
