dd4e9f3e79382b008a3c596f6b5b91413436b88e
EC2 Default User <ec2-user@ip-172-31-26-240.ec2.internal>
2021-03-17 11:44:07 +0000

    [Fix]migration

--- db/migrate/20210315034040_create_products.rb
+++ db/migrate/20210315034040_create_products.rb
@@ -1,17 +1,18 @@
 class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
-      
-      t.integer :genre_id
-      
+
+
+
       t.boolean :is_active, defalut: true
-      
+
       t.string :name
       t.integer :price
       t.text :introduction
       t.string :image_id
 
       t.timestamps
+      t.references :genre, index: true
     end
   end
 end
