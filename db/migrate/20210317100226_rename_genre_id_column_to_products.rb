c54589090e6e217674888ba6db1722ed8107a708
rarayukawa <r1u2u18@gmail.com>
2021-03-17 11:31:04 +0000

    とりあえずコミット

--- db/migrate/20210317100226_rename_genre_id_column_to_products.rb
+++
@@ -1,5 +0,0 @@
-class RenameGenreIdColumnToProducts < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :products, :genre, foreign_key: true
-  end
-end
--- db/migrate/20210317103220_remove_genre_id_from_products.rb
+++
@@ -1,6 +0,0 @@
-class RemoveGenreIdFromProducts < ActiveRecord::Migration[5.2]
-  def change
-    remove_foreign_key :products, :genre_id
-    remove_reference :products, :genre_id, index: true
-  end
-end
