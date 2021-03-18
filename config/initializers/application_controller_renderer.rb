709a5ae56b90da1cd1196b1c2f1da2b5e9fc31e1
rarayukawa <r1u2u18@gmail.com>
2021-03-18 08:58:03 +0000

    商品新規登録追加

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+
+Refile.secret_key = '08c0e998613c3baff248bee72ea829a84710cee4a7d878a98164a8b98a8acde6fd8685d7778f40478ed8912b9c1b5acbd1f13b68810412d55fb33f265eab4c1a'
