fd8dea2222a2400b8bc8d32f2af0670b8c2837c7
EC2 Default User <ec2-user@ip-172-31-90-208.ec2.internal>
2021-03-24 08:53:13 +0000

    [add]注文ステータス機能

--- config/routes.rb
+++ config/routes.rb
@@ -49,7 +49,7 @@ Rails.application.routes.draw do
     resources :genres, only: [:index, :create, :edit, :update]
     resources :customers, only: [:index, :show, :edit, :update]
     resources :orders, only: [:show, :update, :index]
-    resources :orders_details, only: [:update]
+    resources :order_details, only: [:update]
   end
 
 
