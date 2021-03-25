2fc7ed82f65a952adae30df528b30c27b51559d4
EC2 Default User <ec2-user@ip-172-31-90-208.ec2.internal>
2021-03-25 03:00:27 +0000

    とりあえずのコミット

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -3,21 +3,17 @@ class Admin::OrderDetailsController < ApplicationController
   def update
     @order_detail = OrderDetail.find(params[:id])
     @order_detail.update!(order_detail_params)
-    if @order_detail.making_status == "製作中"
-      @order_detail.order.update(status: 2)
+    if @order_detail.making_status = "製作中"
+      @order_detail.order.status.update(status: 2)
     end
-
-
-    order = @order_detail.order
-    # ①[orderに紐づく、order_detailの中でmaking_statusが3のもの]の数
-    making_done_count = order.order_details.where(making_status: 3).count
-    # ①とorderに紐づくorder_detailの数が同じなら、
-    # (= orderに紐づくorder_detailが全て「制作完了」ということに等しい。)
-    if making_done_count == order.order_details.count
-      order.update(status: 3)
+      i = 1
+    while i= @order_detail do
+      if @order_detail.making_status = 3
+         @order_detail.order.status.update(status:3)
+         break
+      end
+      i += 1
     end
-
-  
     redirect_back(fallback_location: admin_orders_path)
   end
 
