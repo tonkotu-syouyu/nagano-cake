c8c008ecf7624aa172eb1a64c4aa90bf64ebe5b6
EC2 Default User <ec2-user@ip-172-31-90-208.ec2.internal>
2021-03-26 02:10:02 +0000

    注文情報入力、確認のレイアウト変更

--- app/models/cart_product.rb
+++ app/models/cart_product.rb
@@ -2,6 +2,8 @@ class CartProduct < ApplicationRecord
   belongs_to :customer
   belongs_to :product
 
+  validates :amount,  presence: true
+
   def total_price
 	  (self.product.price*1.1 * self.amount).to_i
   end
--- app/models/order.rb
+++ app/models/order.rb
@@ -5,15 +5,6 @@ class Order < ApplicationRecord
 
   enum payment_method: { "クレジットカード": 0, "銀行振込": 1 }
   enum status: { "入金待ち": 0, "入金確認": 1, "製作中": 2, "発送準備中": 3, "発送済み": 4 }
-  
-  
-  def total_payment
-  sum = 0
-  self.order_details.each do |order_detail|
-    sum += order_detail.subtotal
-  end
-  sum
-end
 
   def total_payment
     sum = 0
