0b5b1048c1fcb135af24edc2e13e6ce8a31d24bf
EC2 Default User <ec2-user@ip-172-31-90-208.ec2.internal>
2021-03-26 02:13:26 +0000

    数量のバリデーション記載変更

--- app/models/cart_product.rb
+++ app/models/cart_product.rb
@@ -2,7 +2,6 @@ class CartProduct < ApplicationRecord
   belongs_to :customer
   belongs_to :product
 
-  validates :amount,  presence: true
 
   def total_price
 	  (self.product.price*1.1 * self.amount).to_i
